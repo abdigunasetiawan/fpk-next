321d8a54688f9b038278d88c6774e54a83a1b1b6
abdigunasetiawan <abdigunasetiawan0@gmail.com>
2025-09-30 14:00:15 +0700

    fix gsc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,6 @@ export const metadata: Metadata = {
   title: "Forum Programmer Kaputama",
   description:
     "Forum Programmer Kaputama (FPK) adalah organisasi atau komunitas programmer dari kampus Kaputama yang didedikasikan untuk membangun masa depan teknologi. Kami menawarkan pelatihan dalam web development dan desain UI/UX, dengan fokus pada pengembangan keterampilan praktis dan kolaborasi.",
-  verification: {
-    google: "CNwbtIZN48HzXGmu91ShgAX20nhxjp6r6E_2_lY5TPc",
-  },
 };
 
 export default function RootLayout({
@@ -28,7 +25,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head></head>
+      <head>
+        <meta
+          name="google-site-verification"
+          content="lp0j-kJDrPDEHKehxKcbNIPWXLkVJUyU4bSLHDrUOCw"
+        />
+      </head>
       <body
         className={`${inter.variable} font-inter relative flex min-h-screen flex-col overflow-x-hidden antialiased`}
       >
